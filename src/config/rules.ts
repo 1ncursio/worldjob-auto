d010697960edaf7aeb96595107e5b47e19d9ae48
1ncursio <ckswn1323@naver.com>
2023-01-16 14:14:22 +0900

    룰 변경

--- src/config/rules.ts
+++ src/config/rules.ts
@@ -6,10 +6,9 @@ checkInRule.hour = 8
 checkInRule.minute = 51
 checkInRule.dayOfWeek = new schedule.Range(1, 5)
 
-// 매주 평일 22시 0분 => 0 0 22 * * 1-5
 const checkOutRule = new schedule.RecurrenceRule()
-checkOutRule.hour = 22
-checkOutRule.minute = 0
+checkOutRule.hour = 18
+checkOutRule.minute = 55
 checkOutRule.dayOfWeek = new schedule.Range(1, 5)
 
 export { checkInRule, checkOutRule }
